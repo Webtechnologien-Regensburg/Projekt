d90e1d2d524f1021ed41ec33d1eb57c72a9b7364
Martin <martin.kocur@sprachlit.uni-regensburg.de>
2020-07-22 13:41:21 +0200

    neu

--- StarterPaket/app/resources/js/Config.js
+++
@@ -1,7 +0,0 @@
-const Config = {
-	dbBaseRoute: "http://localhost:8080/db/"
-};
-
-Object.freeze(Config);
-
-export default Config;
--- StarterPaket/app/resources/js/app.js
+++
@@ -1,9 +0,0 @@
-/* Das ist der Einstiegspunkt der Anwendung*/
-
-import DatabaseClient from "./database/DatabaseClient.js";
-
-function init() {
-
-}
-
-init();
